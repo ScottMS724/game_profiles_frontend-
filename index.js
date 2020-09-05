394f4e24bcc211c9bf2ab28934992da70fb932a3
scottms724 <scottms724@gmail.com>
2020-09-05 12:32:28 -0400

    HTML markup re-factor with function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const endPoint = "http://localhost:3000/api/v1/games/"
 
 document.addEventListener('DOMContentLoaded', () => {
+    // function call of initial get request of game profiles
     getGameProfiles();
 
     // selecting the create new game profile form
@@ -12,25 +13,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
 })
 
+
+
+// render function for HTML markup of game profiles
+function renderGameProfileHTML(game) {
+    const gameHTMLMarkup = `
+            <div data-id=${game.id}>
+                <h3>${game.attributes.title}</h3>
+                <p>${game.attributes.genre.name}</p>
+                <img src=${game.attributes.image_url} height="200" width="250">
+                <br>
+                <textarea data-id=${game.id} rows="4" cols="50">${game.attributes.review}</textarea>
+                <p>Rating: ${game.attributes.rating}/10</p>
+                <button data-id=${game.id}>Edit</button>
+            </div>
+            <br><br>`;
+
+            document.querySelector('#game-container').innerHTML += gameHTMLMarkup;
+}
+
 // function to get current game profiles
 function getGameProfiles() {
     fetch(endPoint)
     .then(response => response.json())
     .then(games => {
         games.data.forEach(game => {
-            const gameHTMLMarkup = `
-                <div data-id=${game.id}>
-                    <h3>${game.attributes.title}</h3>
-                    <p>${game.attributes.genre.name}</p>
-                    <img src=${game.attributes.image_url} height="200" width="250">
-                    <br>
-                    <textarea data-id=${game.id} rows="4" cols="50">${game.attributes.review}</textarea>
-                    <p>Rating: ${game.attributes.rating}/10</p>
-                    <button data-id=${game.id}>Edit</button>
-                </div>
-                <br><br>`;
-
-                document.querySelector('#game-container').innerHTML += gameHTMLMarkup;
+            renderGameProfileHTML(game); 
         })
     })
 }
@@ -59,19 +67,7 @@ function postFetch(title, genre_id, image_url, review, rating) {
     .then(game => {
          const gameData = game.data;
 
-         const gameHTMLMarkup = `
-                <div data-id=${game.id}>
-                    <h3>${gameData.attributes.title}</h3>
-                    <p>${gameData.attributes.genre.name}</p>
-                    <img src=${gameData.attributes.image_url} height="200" width="250">
-                    <br>
-                    <textarea data-id=${game.id} rows="4" cols="50">${gameData.attributes.review}</textarea>
-                    <p>Rating: ${gameData.attributes.rating}/10</p>
-                    <button data-id=${game.id}>Edit</button>
-                </div>
-                <br><br>`;
-
-                document.querySelector("#game-container").innerHTML += gameHTMLMarkup;
+         renderGameProfileHTML(gameData);
     })
 }
 
