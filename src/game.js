810a70eeeee4b15a454dafbc57be24ee371a0772
scottms724 <scottms724@gmail.com>
2020-09-26 14:46:45 -0400

    final CSS implemented

--- src/game.js
+++ src/game.js
@@ -14,23 +14,24 @@ class Game {
     renderGameProfileHTML() {
     return `
       <div data-id="${this.id} class="card">
-        <div class="col-md-4">
+        <div class="col-md-12">
           <div class="card mb-4 shadow-sm">
+          <div class="card text-center">
+          <div class="card text-white bg-dark mb-3"
             <div class="card-body">
 
               <img src="${this.image_url}" class="card-img-top" alt="...">
 
-              <u><h5 class="card-title">${this.title}</h5></u>
+              <u><h1 class="card-title">${this.title}</h1></u>
 
               <p class="card-text">${this.review}</p>
               
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button type="button" class="btn btn-primary">Edit</button>
                   <button type="button" data-id="${this.id}" class="btn btn-danger">Delete! <br><small>click twice</small><br></button>
                 </div>
 
-                <small class="text-muted">
+                <small>
                 Genre: ${this.genre.name}
                 <br>
                 My Rating: ${this.rating}/10
@@ -41,6 +42,8 @@ class Game {
           </div>
         </div>
       </div>
+    </div>
+    </div>
     </div>
     `
    
